335e792e8b846d8dde92910834768854b5b64ba2
redswampMO <ikirtivardhansingh1@gmail.com>
2026-04-08 16:58:33 +0530

    update

--- RedSwampMO--/src/utils/random.js
+++
@@ -1,7 +0,0 @@
-export const getRandomNumber = (forceOdd = false) => {
-  if (forceOdd) {
-    const odds = [1, 3, 5];
-    return odds[Math.floor(Math.random() * odds.length)];
-  }
-  return Math.floor(Math.random() * 6) + 1;
-};
